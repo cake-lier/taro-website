d9558cb5dbb7043facca2219dd0b04b4ad1c05cc
Matteo Castellucci <matteo.castellucci@outlook.com>
2024-12-08 20:53:28 +0100

    ci: add test and release ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import zipPack from "vite-plugin-zip-pack";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), zipPack()],
 });
