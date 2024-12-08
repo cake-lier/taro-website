05be92e62f1e9bf09db7e6820d3f0cf21bbfeffc
Matteo Castellucci <matteo.castellucci@outlook.com>
2024-12-08 19:38:01 +0100

    refactor: translate strings left untranslated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 import { Copyright, Favorite } from "@mui/icons-material";
 import { Button, Grid2 as Grid, Typography } from "@mui/material";
-import { Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 
 export default function Footer() {
+  const { i18n } = useTranslation();
+  const format = new Intl.DateTimeFormat(i18n.language, { year: "numeric" });
+
   return (
     <Grid container justifyContent="end" sx={{ marginBottom: "10px" }}>
       <Grid container alignItems="center">
@@ -10,20 +13,22 @@ export default function Footer() {
           <Typography variant="body2">Made with</Typography>
           <Favorite sx={{ margin: "0 4px", fontSize: "1.25rem" }} htmlColor="#FF0000" />
           <Typography variant="body2">by</Typography>
+          <Button
+            sx={{ padding: 0, margin: "0 0 0 4px", textTransform: "none" }}
+            disableRipple
+            variant="text"
+            href="https://github.com/cake-lier"
+            target="_blank"
+          >
+            cake-lier
+          </Button>
         </Trans>
-        <Button
-          sx={{ padding: 0, margin: "0 0 0 4px", textTransform: "none" }}
-          disableRipple
-          variant="text"
-          href="https://github.com/cake-lier"
-          target="_blank"
-        >
-          cake-lier
-        </Button>
       </Grid>
       <Grid container alignItems="center">
         <Copyright sx={{ margin: "0 4px", fontSize: "1.25rem" }} />
-        <Typography variant="body2">2024 - {new Date().getFullYear()}</Typography>
+        <Typography variant="body2">
+          {format.format(new Date("2024-11-27"))} - {format.format(Date.now())}
+        </Typography>
       </Grid>
     </Grid>
   );
