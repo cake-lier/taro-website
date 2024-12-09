78c1b909211b192beea976cab37fc929ca175ce3
Matteo Castellucci <matteo.castellucci@outlook.com>
2024-12-09 22:01:42 +0100

    fix: add padding to title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -8,7 +8,7 @@ export default function Title({ adjectiveIndex }: { adjectiveIndex: number }) {
     <AppBar position="relative" color="transparent" sx={{ boxShadow: "none" }}>
       <Toolbar sx={{ padding: "0 !important" }}>
         <Button disableRipple variant="text" sx={{ textTransform: "none", padding: "0", marginTop: "10px" }} href="/">
-          <Typography variant="h5">
+          <Typography variant="h5" sx={{ padding: "6px 8px" }}>
             {t("header.title", { adjective: t(`header.adjectives.${adjectiveIndex.toString()}`) })}
           </Typography>
         </Button>
