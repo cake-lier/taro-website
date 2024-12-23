3009cd1883ea3ced682cb3ecf646767827ec725e
Matteo Castellucci <matteo.castellucci@outlook.com>
2024-12-23 02:29:17 +0100

    feat: add new pride art

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,7 +4,7 @@ import Carousel from "react-material-ui-carousel";
 export default function Gallery() {
   return (
     <Carousel animation="slide" height="50vh" sx={{ marginBottom: "10px" }}>
-      {["IMG_2477.PNG", "IMG_7956.PNG"].map((p, i) => (
+      {["ref_sheet.png", "japan.png", "pride.jpg"].map((p, i) => (
         <Box key={i} component="img" sx={{ height: 1, width: 1, objectFit: "contain" }} src={`/img/${p}`} />
       ))}
     </Carousel>
